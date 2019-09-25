83b285522eef097af6fd9270da83c2541d4512b1
rishinath4159 <rishi.nath@gmail.com>
2019-09-24 17:35:15 -0700

    clean code

--- public/index.js
+++ public/index.js
@@ -6,12 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
         socket.emit("typed", event.keyCode);
     })
 
-    socket.on("update", function(str) {
+    socket.on("new-string", function(str, word) {
         appendable.innerHTML = str;
-        appendable.style.color = "#f4f1d6";
-    })
-
-    socket.on("word", function() {
-        appendable.style.color = "green";
+        appendable.style.color = word ? "#32CD32" : "#f4f1d6";
     })
 });
--- server.js
+++ server.js
@@ -5,38 +5,38 @@ const express = require("express");
 const request = require("request");
 
 let mstr = "";
-
 let words = [];
+let word = false;
 
 // TODO: Find better word list
 request("https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa.txt", (err, res, body) => {
     words = body.split("\n").filter(word => word.length > 3);
 })
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.use(express.static("public"));
 
-io.on("connection", function(socket){
-  socket.on("typed", function(code) {
+io.on("connection", function(socket) {
+    socket.emit("new-string", mstr, word);
+
+    socket.on("typed", function(code) {
       let char = String.fromCharCode(code);
+
       if (code === 8) {
           mstr = mstr.slice(0, -1);
-          io.emit("update", mstr);
-      } else
-      if (char.toUpperCase() != char.toLowerCase()) {
+      } else if (char.toUpperCase() != char.toLowerCase()) {
           mstr += char;
-          io.emit("update", mstr);
       }
 
-      if (words.includes(mstr.toLowerCase())) {
-          io.emit("word")
-      }
+      word = words.includes(mstr.toLowerCase())
+
+      io.emit("new-string", mstr, word);
   })
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log("serving on *:3000");
 });
