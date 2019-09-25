8d1251a15e98b30cb21bddfc6f40338367107671
rishinath4159 <rishi.nath@gmail.com>
2019-09-24 17:54:21 -0700

    rudimentary turn system

--- public/index.js
+++ public/index.js
@@ -2,12 +2,23 @@ document.addEventListener("DOMContentLoaded", function() {
     let socket = io();
     let appendable = document.getElementById("appendable");
 
+    let id = 0;
+
     document.addEventListener("keydown", function(event) {
-        socket.emit("typed", event.keyCode);
+        socket.emit("typed", event.keyCode, id);
+    })
+
+    socket.on("connection", function(str, word, m_id) {
+        id = m_id;
+        update(str, word);
     })
 
     socket.on("new-string", function(str, word) {
+        update(str, word);
+    })
+
+    const update = function(str, word) {
         appendable.innerHTML = str;
         appendable.style.color = word ? "#32CD32" : "#f4f1d6";
-    })
+    }
 });
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const request = require("request");
 let mstr = "";
 let words = [];
 let word = false;
+let turn = 1;
 
 // TODO: Find better word list
 request("https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa.txt", (err, res, body) => {
@@ -20,20 +21,28 @@ app.get("/", function(req, res) {
 app.use(express.static("public"));
 
 io.on("connection", function(socket) {
-    socket.emit("new-string", mstr, word);
+    id = Object.keys(io.sockets.clients().sockets).length;
 
-    socket.on("typed", function(code) {
-      let char = String.fromCharCode(code);
+    socket.emit("connection", mstr, word, id);
 
-      if (code === 8) {
-          mstr = mstr.slice(0, -1);
-      } else if (char.toUpperCase() != char.toLowerCase()) {
-          mstr += char;
-      }
+    socket.id = id;
 
-      word = words.includes(mstr.toLowerCase())
+    socket.on("typed", function(code, s_id) {
+        if (turn != s_id) return;
 
-      io.emit("new-string", mstr, word);
+        let char = String.fromCharCode(code);
+
+        if (code === 8) {
+            mstr = mstr.slice(0, -1);
+        } else if (char.toUpperCase() != char.toLowerCase()) {
+            mstr += char;
+        }
+
+        word = words.includes(mstr.toLowerCase())
+
+        io.emit("new-string", mstr, word);
+        
+        turn = (turn % Object.keys(io.sockets.clients().sockets).length) + 1
   })
 });
 
